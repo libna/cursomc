9879f6dee767435e8b802e2fc5cf79762a782c76
libna <libnarodrigues723@gmail.com>
2020-05-05 09:39:14 -0300

    Testando a primeira classe de dominio(categoria)

--- src/main/java/com/libna/mc/resources/CategoriaResource.java
+++ src/main/java/com/libna/mc/resources/CategoriaResource.java
@@ -1,14 +1,25 @@
 package com.libna.mc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.libna.mc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	 @RequestMapping(method=RequestMethod.GET)
-	 public String listar() {
-		 return "REST está funcionando";
+	 public List<Categoria> listar() {
+		 
+		 Categoria cat1 = new Categoria(1,"informatica");
+		 Categoria cat2 = new Categoria(2,"escritorio");
+		 List<Categoria> lista = new ArrayList<Categoria>();
+		 lista.add(cat1);
+		 lista.add(cat2);
+		 return lista;
 	 }
 }
