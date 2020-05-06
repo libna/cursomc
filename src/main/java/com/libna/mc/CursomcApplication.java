4e078080794fbbffce74c8d42c0228914b9b534c
libna <libnarodrigues723@gmail.com>
2020-05-06 20:21:42 -0300

    criando operação de instanciação

--- src/main/java/com/libna/mc/CursomcApplication.java
+++ src/main/java/com/libna/mc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.libna.mc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.libna.mc.domain.Categoria;
+import com.libna.mc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+		@Autowired
+		private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"informatica");
+		Categoria cat2 = new Categoria(null,"escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
+	}
+
 }
+
